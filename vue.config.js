c49b34612d7b48bfd76bab34ae2ac55be8764f14
bobthepoet <wangyinsheng@MBA.local>
2021-12-06 16:10:14 +0800

    baic profile

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+let proxyObj = {};
+
+proxyObj['/'] = {
+  //websocket
+  ws: false,
+  //目标地址
+  target: 'http://dongeast.top:8181',
+  //发送请求头中host会设置成target
+  changeOrigin: true,
+  //不重写请求地址
+  pathRewrite: {
+    '^/': '/'
+  }
+};
+
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: proxyObj
+  }
+};
